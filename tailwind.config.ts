aa8e780099c8f6f125139e4f81d6c61fc3d56dd1
dashdashtomscratch <dashdashtomscratch@gmail.com>
2025-06-07 06:44:16 +0000

    Commiting a retry

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,18 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from 'tailwindcss'
 
 export default {
-  content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        'mono': ['Lato', 'ui-monospace', 'monospace'],
+        'display': ['Arvo', 'ui-monospace', 'monospace'],
+        'serif': ['Literata Variable', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
   ],
-} satisfies Config;
+} as Config
